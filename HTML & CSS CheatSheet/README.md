ce37e2fea1f6a3589fc94a503958ed40bc10b360
Atanu <atanum991@gmail.com>
2024-01-22 15:52:56 +0530

    mod

--- HTML & CSS CheatSheet/README.md
+++
@@ -1,2 +0,0 @@
-# My-dev-space
-This cheat sheet contains all the topics of html and css.
--- JavaScript CheatSheet/test.js
+++
@@ -1,5 +0,0 @@
-let arr=[10,20,35,60,55]
-let a=arr.map((value)=>{
-	return value+value*value-value/value; 
-})
-console.log(a);
